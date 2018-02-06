0d9c0e79b384fb7ef05ad5df020530cefa2650df
Dan F-M <danfm@nyu.edu>
2018-02-05 20:38:49 -0500

    updating

--- get_pubs.py
+++ get_pubs.py
@@ -50,7 +50,8 @@ def get_papers(author):
             volume=paper.volume,
             page=page,
             arxiv=aid[0] if len(aid) else None,
-            citations=paper.citation_count,
+            citations=(paper.citation_count
+                       if paper.citation_count is not None else 0),
             url="http://adsabs.harvard.edu/abs/" + paper.bibcode,
         ))
     return sorted(dicts, key=itemgetter("pubdate"), reverse=True)
